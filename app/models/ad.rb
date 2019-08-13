1c189172882788639d5e995543a96a4fddb2a2cb
Rubem Vasconcelos <rfsv@ic.ufal.br>
2019-08-13 15:57:24 -0300

    Ads details

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -6,7 +6,7 @@ class Ad < ApplicationRecord
   validates :title, :description, :category, :picture, :finish_date, presence: true
   validates :price, numericality: {greater_than: 0}
   # paperclip
-  has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   scope :desc_order, ->(quantity = 10) { limit(quantity).order(created_at: :desc) }
