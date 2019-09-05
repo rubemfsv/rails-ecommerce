ba43f4548fca9937a831ead2ba2f1b867aac8496
Rubem Vasconcelos <rfsv@ic.ufal.br>
2019-09-05 13:45:44 -0300

    Comments added

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
     end
 
     resources :ad_detail, only: [:show]
-    resources :categories, only: [:show ]
+    resources :categories, only: [:show]
+    resources :comments, only: [:create]
   end
 
   devise_for :admins, :skip => [:registrations]
